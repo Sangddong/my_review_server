3eeace98e2dc0866df5a78b1c6725ce36105d9ef
sangddong <hehe74029@gmail.com>
2026-08-10 11:55:08 +0900

    init

---
+++ src/main/java/com/example/myreviewserver/MyReviewServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myreviewserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReviewServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReviewServerApplication.class, args);
+	}
+
+}
